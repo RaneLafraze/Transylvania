d9da24fb41938cff2d012d05acae8109e1bbabb6
Rane Lafraze <lafrazerl@gmail.com>
2016-07-03 20:43:45 -0400

    Initialize project

---
+++ TransylvaniaGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TransylvaniaGame
+//
+//  Created by Seth Byrne on 7/3/16.
+//  Copyright © 2016 Intriguing Entertainment. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

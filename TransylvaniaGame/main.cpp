8c4d9b502944683e785f850efed9203dd12e079e
Rane Lafraze <lafrazerl@gmail.com>
2016-07-03 20:47:12 -0400

    Remove comments and generated code. Fix formatting

--- TransylvaniaGame/main.cpp
+++ TransylvaniaGame/main.cpp
@@ -1,15 +1,12 @@
-//
-//  main.cpp
-//  TransylvaniaGame
-//
-//  Created by Seth Byrne on 7/3/16.
-//  Copyright © 2016 Intriguing Entertainment. All rights reserved.
-//
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main(int argc, const char * argv[])
+{
+    
+    printf("Welcome to this amazing game, TransylvaniaGame!\n");
+    
     return 0;
 }
